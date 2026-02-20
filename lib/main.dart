1d9654d561ce1526469eb7eca3d943319831edf0
amineze1605-cloud <amineze1605@gmail.com>
2026-02-20 04:57:25 +0100

    Initialize Flutter app with login screen

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(HashLedgerApp());
+}
+
+class HashLedgerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HashLedger',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginScreen(),
+    );
+  }
+}
