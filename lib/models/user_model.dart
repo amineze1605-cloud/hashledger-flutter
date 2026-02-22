520a162936118a882d8f53828bcf2b1dca14f090
amineze1605-cloud <amineze1605@gmail.com>
2026-02-22 20:34:12 +0100

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -4,9 +4,15 @@ class UserModel {
   final int points;
   final String token;
 
-  UserModel({required this.id, required this.email, required this.points, required this.token});
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.points,
+    required this.token,
+  });
 
-  factory UserModel.fromJson(Map<String, dynamic> json, String token) {
+  factory UserModel.fromJson(
+      Map<String, dynamic> json, String token) {
     return UserModel(
       id: json['id'],
       email: json['email'],
