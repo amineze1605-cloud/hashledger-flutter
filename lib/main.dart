089fdf4d82bf07cb7d5cc734e07988840bc5f682
amineze1605-cloud <amineze1605@gmail.com>
2026-04-06 16:23:59 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,90 @@
 import 'package:flutter/material.dart';
+import 'models/user_model.dart';
+import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
+import 'services/api_service.dart';
+import 'services/storage_service.dart';
 
 void main() {
-  runApp(HashLedgerApp());
+  runApp(const HashLedgerApp());
 }
 
 class HashLedgerApp extends StatelessWidget {
+  const HashLedgerApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HashLedger',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AppStartupScreen(),
     );
+  }
+}
+
+class AppStartupScreen extends StatefulWidget {
+  const AppStartupScreen({super.key});
+
+  @override
+  State<AppStartupScreen> createState() => _AppStartupScreenState();
+}
+
+class _AppStartupScreenState extends State<AppStartupScreen> {
+  bool loading = true;
+  UserModel? user;
+
+  @override
+  void initState() {
+    super.initState();
+    checkSession();
+  }
+
+  Future<void> checkSession() async {
+    final token = await StorageService.getToken();
+
+    if (token == null || token.isEmpty) {
+      if (!mounted) return;
+      setState(() {
+        loading = false;
+      });
+      return;
+    }
+
+    final result = await ApiService.getUser(token);
+
+    if (!mounted) return;
+
+    if (result.containsKey("error")) {
+      await StorageService.clearToken();
+      setState(() {
+        loading = false;
+      });
+      return;
+    }
+
+    setState(() {
+      user = UserModel.fromJson(result, token);
+      loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (loading) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    if (user != null) {
+      return HomeScreen(user: user!);
+    }
+
+    return const LoginScreen();
   }
 }
